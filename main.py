27d5c017ed98b7b3e531601520f44a3bc2308123
Kirill <mkir2006@gmail.com>
2023-02-16 16:01:57 +0300

    Update code

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from roman import *
 
-Arabic_Numbers = ['IV', 'LVIII', 'MCMXCIV', 'XCIX', 'LXXX', 'LXIX']
+Arabic_Numbers = [str(i) for i in input('Римское число для преобразования в арабское: ').split()]
 print(Arabic_Numbers)
 for i in Arabic_Numbers:
     print(i, '->' ,roman_to_int(i))
 
-Numbers =[4, 58, 1994, 26, 99, 69, 80]
+Numbers = [int(i) for i in input('Арабское число для преобразования в римское: ').split()]
+#[4, 58, 1994, 26, 99, 69, 80]
 for i in Numbers:
     print(i, '->',int_to_roman(i))
