52ce997c122118fda19017c4a43069cf05068403
Kirill <mkir2006@gmail.com>
2023-02-16 15:55:23 +0300

    Update names

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from roman import *
 
-t = ['IV', 'LVIII', 'MCMXCIV', 'XCIX', 'LXXX', 'LXIX']
-print(t)
-for i in t:
+Arabic_Numbers = ['IV', 'LVIII', 'MCMXCIV', 'XCIX', 'LXXX', 'LXIX']
+print(Arabic_Numbers)
+for i in Arabic_Numbers:
     print(i, '->' ,roman_to_int(i))
 
-a=[4, 58, 1994, 26, 99, 69, 80]
-for i in a:
+Numbers =[4, 58, 1994, 26, 99, 69, 80]
+for i in Numbers:
     print(i, '->',int_to_roman(i))
